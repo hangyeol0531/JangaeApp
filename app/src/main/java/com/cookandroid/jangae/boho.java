c1d56b8eff0d2b8802f639679dacffae8195727d
hangyeol0531 <41174265+hangyeol0531@users.noreply.github.com>
2019-12-09 17:43:38 +0900

    asd

--- app/src/main/java/com/cookandroid/jangae/boho.java
+++ app/src/main/java/com/cookandroid/jangae/boho.java
@@ -2,7 +2,9 @@ package com.cookandroid.jangae;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class boho extends Activity {
@@ -16,6 +18,15 @@ public class boho extends Activity {
         name = intent.getStringExtra("name");
         tv1 = (TextView)findViewById(R.id.nameline);
         tv1.setText(name + "님 환영합니다!!!");
+    }
 
+    public void onButton1Clicked(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://m.shinhan.ac.kr/community/notice/view.php?idx=5033&board_id=NACOMM11&category=")); startActivity(intent);
+    }
+
+    public void onButton2Clicked(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("https://m.blog.naver.com/PostView.nhn?blogId=chungong1&logNo=221270206131&proxyReferer=https%3A%2F%2Fwww.google.com%2F")); startActivity(intent);
     }
 }
--- app/src/main/java/com/cookandroid/jangae/fl1.java
+++ app/src/main/java/com/cookandroid/jangae/fl1.java
@@ -1,6 +1,7 @@
 package com.cookandroid.jangae;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -54,6 +55,17 @@ public class fl1 extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+
+    public void onButton1Clicked(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+        Uri.parse("http://m.shinhan.ac.kr/community/notice/view.php?idx=5033&board_id=NACOMM11&category=")); startActivity(intent);
+    }
+
+    public void onButton2Clicked(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+        Uri.parse("https://m.blog.naver.com/PostView.nhn?blogId=chungong1&logNo=221270206131&proxyReferer=https%3A%2F%2Fwww.google.com%2F")); startActivity(intent);
     }
 }
 
